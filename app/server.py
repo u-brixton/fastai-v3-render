99cca2bfbb757e6890680257bc58632ca0a0cbfe
zayashnikovre <un0blogger@gmail.com>
2019-10-26 21:36:50 +0300

    simple

--- app/server.py
+++ app/server.py
@@ -65,7 +65,7 @@ async def analyze(request):
     # img_bytes = await (img_data['file'].read())
     # img = open_image(BytesIO(img_bytes))
     # prediction = learn.predict(img)[0]
-    return JSONResponse({'result': [{"id": 1, "name": 'alex'}]})
+    return JSONResponse({'result': 'heyoo'})
 
 
 if __name__ == '__main__':
