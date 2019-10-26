c1da7f725da382cd88096f90ac420ed361c470d3
zayashnikovre <un0blogger@gmail.com>
2019-10-26 21:30:49 +0300

    changed response

--- app/server.py
+++ app/server.py
@@ -61,11 +61,11 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': [{id: 1, name: 'alex'}]})
+    # img_data = await request.form()
+    # img_bytes = await (img_data['file'].read())
+    # img = open_image(BytesIO(img_bytes))
+    # prediction = learn.predict(img)[0]
+    return JSONResponse({'result': [{"id": 1, "name": 'alex'}]})
 
 
 if __name__ == '__main__':
